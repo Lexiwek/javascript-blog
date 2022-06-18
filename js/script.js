74e086a21950b9a2469adced7bb0f956c700440a
Marek Miśkowiec <miskowiec.ma@gmail.com>
2022-06-18 11:21:45 +0200

    Add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ document.getElementById('test-button').addEventListener('click', function(){
   */
 
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
 
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -18,6 +18,7 @@ document.getElementById('test-button').addEventListener('click', function(){
       activeLink.classList.remove('active');
     }
     /* add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
 
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
